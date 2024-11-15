8f3f1e6d8f08d92da267be723c3bdd9d8f1b2f0a
jamshidqayimov <jamshidqayimov0399@gmail.com>
2024-11-15 21:46:43 +0500

    new

--- app/script.js
+++ app/script.js
@@ -2,11 +2,17 @@ let isSequential = true;
 let currentIndex = 0;
 
 // Random indekslarni saqlash uchun o'zgaruvchilar
+let randomSimplesIndex = 0;
 let randomNounIndex = 0;
 let randomAdjectiveIndex = 0;
 let randomVerbIndex = 0;
 
 // HTML elementlar
+const simpleWordElement = document.getElementById("simpleWord");
+const simpleTranslationElement = document.getElementById("simpleTranslation");
+const simpleTranslationBtn = document.getElementById("simpleTranslationBtn");
+const simpleListElement = document.getElementById("simpleList");
+
 const nounWordElement = document.getElementById("nounWord");
 const nounTranslationElement = document.getElementById("nounTranslation");
 const nounTranslationBtn = document.getElementById("nounTranslationBtn");
@@ -35,15 +41,20 @@ document.body.appendChild(nextWordBtn);
 // Funksiya: So'zlarni ko'rsatish
 function showWords() {
   if (isSequential) {
+    randomSimplesIndex = currentIndex;
     randomNounIndex = currentIndex;
     randomAdjectiveIndex = currentIndex;
     randomVerbIndex = currentIndex;
   } else {
+    randomSimplesIndex = Math.floor(Math.random() * simples.length);
     randomNounIndex = Math.floor(Math.random() * nouns.length);
     randomAdjectiveIndex = Math.floor(Math.random() * adjectives.length);
     randomVerbIndex = Math.floor(Math.random() * verbs.length);
   }
 
+  simpleWordElement.innerText = `Sikl: ${randomSimplesIndex + 1}. ${
+    simples[randomSimplesIndex].english
+  }`;
   nounWordElement.innerText = `Ot: ${randomNounIndex + 1}. ${
     nouns[randomNounIndex].english
   }`;
@@ -54,12 +65,18 @@ function showWords() {
     verbs[randomVerbIndex].english
   }`;
 
+  simpleTranslationElement.innerText = "";
   nounTranslationElement.innerText = "";
   adjectiveTranslationElement.innerText = "";
   verbTranslationElement.innerText = "";
 }
 
 // Tarjimalarni ko'rsatish tugmalari
+simpleTranslationBtn.addEventListener("click", () => {
+  const index = currentIndex % simples.length;
+  simpleTranslationElement.innerText = `Tarjima: ${simples[randomSimplesIndex].uzbek}`;
+});
+
 nounTranslationBtn.addEventListener("click", () => {
   nounTranslationElement.innerText = `Tarjima: ${nouns[randomNounIndex].uzbek}`;
 });
@@ -93,6 +110,16 @@ document.getElementById("randomBtn").addEventListener("click", () => {
 });
 
 // Ro'yxatlarni ko'rsatish funksiyalari
+
+function showSimpleList() {
+  simpleListElement.innerHTML = simples
+    .map(
+      (simple, index) =>
+        `<p>${index + 1}. ${simple.english} - ${simple.uzbek}</p>`
+    )
+    .join("");
+  simpleListElement.style.display = "block";
+}
 function showNounList() {
   nounListElement.innerHTML = nouns
     .map(
@@ -120,25 +147,26 @@ function showVerbList() {
 
 function toggleAllLists() {
   const isAnyListVisible =
+    simpleListElement.style.display === "block" ||
     nounListElement.style.display === "block" ||
     adjectiveListElement.style.display === "block" ||
     verbListElement.style.display === "block";
 
   if (isAnyListVisible) {
+    simpleListElement.style.display = "none";
     nounListElement.style.display = "none";
     adjectiveListElement.style.display = "none";
     verbListElement.style.display = "none";
   } else {
+    showSimpleList();
     showNounList();
     showAdjectiveList();
     showVerbList();
   }
 }
 
-// Ro'yxatlarni ko'rsatish/yashirish tugmasi
 document
   .getElementById("showAllListsBtn")
   .addEventListener("click", toggleAllLists);
 
-// Dastlabki so'zlarni ko'rsatish
 showWords();
